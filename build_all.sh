faaccf4b2feb0e7232ff3d3b4d8b7f837cac092b
Hung Dang <hungptit@gmail.com>
2020-12-14 16:55:05 -0500

    Fix the Windows build issue.

--- build_all.sh
+++ build_all.sh
@@ -11,6 +11,6 @@ git submodule update --init
 popd || exit
 
 # We need to disable ABSL installation because of this issue https://github.com/grpc/grpc/issues/24976
-./build_using_cmake.sh grpc -DABSL_ENABLE_INSTALL=OFF -DgRPC_PROTOBUF_PACKAGE_TYPE=MODULE
+./build_using_cmake.sh grpc -DABSL_ENABLE_INSTALL=OFF -DgRPC_PROTOBUF_PACKAGE_TYPE=module -DgRPC_PROTOBUF_PROVIDER=module
 
 popd || exit
