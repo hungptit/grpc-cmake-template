125ec366189c994e3d7d7747e1976eff3aae7aeb
Hung Dang <hungptit@gmail.com>
2020-12-14 23:42:27 -0500

    Update the arch argument.

--- build_all.ps1
+++ build_all.ps1
@@ -19,12 +19,12 @@ git submodule update --init
 cd $root_path
 mkdir build
 cd build
-cmake -G "Visual Studio 14 2015 Win64" $grpc_path -DABSL_ENABLE_INSTALL=OFF -DCMAKE_INSTALL_PREFIX="$third_party_path" -DgRPC_PROTOBUF_PACKAGE_TYPE=MODULE -DgRPC_PROTOBUF_PROVIDER=module
+cmake -G "Visual Studio 14 2015" -A x64 $grpc_path -DABSL_ENABLE_INSTALL=OFF -DCMAKE_INSTALL_PREFIX="$third_party_path" -DgRPC_PROTOBUF_PACKAGE_TYPE=MODULE -DgRPC_PROTOBUF_PROVIDER=module
 cmake --build . --config $config -- /maxcpucount
 cmake --build . --config $config --target install
 
 # Build examples
 cd $root_path
-cmake -G "Visual Studio 14 2015 Win64" .
+cmake -G "Visual Studio 14 2015"  -A x64 .
 cmake --build . --config $config -- /maxcpucount
 
