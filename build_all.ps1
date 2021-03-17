49e67cc627e5e07b433d433f7956564519000a9a
Hung Dang <hdang@tableau.com>
2021-03-17 15:11:10 -0700

    Fix build failure.

--- build_all.ps1
+++ build_all.ps1
@@ -14,20 +14,18 @@ Write-Output "vs_version: $vs_version"
 
 # Get grpc source
 cd $third_party_path
-cmake -G "$vs_version" -A x64 .
-
-
+cmake -G "Visual Studio 14 2015" -A x64 .
 
 # Build grpc
 cd $root_path
 mkdir build
 cd build
-cmake -G "$vs_version" -A x64 $grpc_path -DABSL_ENABLE_INSTALL=OFF -DCMAKE_INSTALL_PREFIX="$third_party_path" -DgRPC_PROTOBUF_PACKAGE_TYPE=MODULE -DgRPC_PROTOBUF_PROVIDER=module
+cmake -G "Visual Studio 14 2015" -A x64 $grpc_path -DABSL_ENABLE_INSTALL=OFF -DCMAKE_INSTALL_PREFIX="$third_party_path" -DgRPC_PROTOBUF_PACKAGE_TYPE=MODULE -DgRPC_PROTOBUF_PROVIDER=module
 cmake --build . --config $config -- /maxcpucount
 cmake --build . --config $config --target install
 
 # Build examples
 cd $root_path
-cmake -G "$vs_version"  -A x64 .
+cmake -G "Visual Studio 14 2015"  -A x64 .
 cmake --build . --config $config -- /maxcpucount
 
