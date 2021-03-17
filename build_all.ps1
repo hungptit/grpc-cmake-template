8b0336653a93d3f8e7de006feb8fd79dd54936a0
Hung Dang <hdang@tableau.com>
2021-03-17 15:09:27 -0700

    Minor update.

--- build_all.ps1
+++ build_all.ps1
@@ -5,27 +5,29 @@ param (
 $root_path = Get-Location
 $third_party_path = "$root_path\3p"
 $grpc_path = "$third_party_path\3p\_deps\grpc-src\"
+vs_version = "Visual Studio 14 2015"
 
 Write-Output "root_path: $root_path"
 Write-Output "third_party_path: $third_party_path"
 Write-Output "grpc_path: $grpc_path"
-
-vs_version = "Visual Studio 14 2015"
+Write-Output "vs_version: $vs_version"
 
 # Get grpc source
 cd $third_party_path
-cmake -G $vs_version -A x64 .
+cmake -G "$vs_version" -A x64 .
+
+
 
 # Build grpc
 cd $root_path
 mkdir build
 cd build
-cmake -G $vs_version -A x64 $grpc_path -DABSL_ENABLE_INSTALL=OFF -DCMAKE_INSTALL_PREFIX="$third_party_path" -DgRPC_PROTOBUF_PACKAGE_TYPE=MODULE -DgRPC_PROTOBUF_PROVIDER=module
+cmake -G "$vs_version" -A x64 $grpc_path -DABSL_ENABLE_INSTALL=OFF -DCMAKE_INSTALL_PREFIX="$third_party_path" -DgRPC_PROTOBUF_PACKAGE_TYPE=MODULE -DgRPC_PROTOBUF_PROVIDER=module
 cmake --build . --config $config -- /maxcpucount
 cmake --build . --config $config --target install
 
 # Build examples
 cd $root_path
-cmake -G $vs_version  -A x64 .
+cmake -G "$vs_version"  -A x64 .
 cmake --build . --config $config -- /maxcpucount
 
