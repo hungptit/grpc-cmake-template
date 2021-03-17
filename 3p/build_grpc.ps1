96c8b7ee164b8ec269b2719f242322a72dd7a664
Hung Dang <hdang@tableau.com>
2021-03-17 14:23:11 -0700

    Try to hard code CMake path.

--- 3p/build_grpc.ps1
+++ 3p/build_grpc.ps1
@@ -2,7 +2,7 @@ param (
     [Parameter(Mandatory=$true)][ValidateSet('Release', 'Debug')]$config
 )
 
-cmake -G "Visual Studio 14 2015" -A x64 $grpc_path -DCMAKE_INSTALL_PREFIX="$PWD"
-cmake --build . --config $config -- /maxcpucount
-cmake --build . --config $config --target install
+& 'C:\Program Files\CMake\bin\cmake.exe' -G "Visual Studio 14 2015" -A x64 $grpc_path -DCMAKE_INSTALL_PREFIX="$PWD"
+& 'C:\Program Files\CMake\bin\cmake.exe' --build . --config $config -- /maxcpucount
+& 'C:\Program Files\CMake\bin\cmake.exe' --build . --config $config --target install
 
