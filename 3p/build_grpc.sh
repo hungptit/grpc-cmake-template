be078793581361d63a80d18af77596b33d8725ec
Hung Dang <hungptit@gmail.com>
2022-03-05 17:49:13 -0500

    Minor cleanup

--- 3p/build_grpc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-config=${1:"Release"}
-root_dir="$PWD"
-src_dir="$root_dir/_deps/grpc-src"
-dst_dir="$root_dir/.local"
-cmake ./ -DCMAKE_BUILD_TYPE="$config" -DCMAKE_INSTALL_PREFIX="$dst_dir"
-make -j9
-make install
-
